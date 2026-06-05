16dce4f4588fda9681d41de48a70de13b9637447
Haider Malik <haidermalik504@gmail.com>
2026-06-05 21:09:35 +0500

    completed temprature

--- src/index.js
+++ src/index.js
@@ -1,49 +1,26 @@
 import {openai} from './openai.js'
-import {encoding_for_model} from 'tiktoken';
+// import {encoding_for_model} from 'tiktoken';
 
 async function main(){
-   
-    // const response = await openai.responses.create({
-    //     input: '10*10*23',
-    //     instructions: 'Give result in 1 word',
-    //     model: 'gpt-4o-mini',
-    // });
 
-
-    // const response = await openai.responses.create({
-    //     input: 'tell mw one best color',
-    //     instructions: 'Give result in 20 word',
-    //     model: 'gpt-4o-mini',
-    // });
-
-
-    const prompt = `Many words map to one token, but some don't: indivisible.
-
-Unicode characters like emojis may be split into many tokens containing the underlying bytes: 🤚🏾
-
-Sequences of characters commonly found next to each other may be grouped together: 1234567890`;
+    const prompt = `How are you?`;
     const model = 'gpt-4o-mini'
 
 
     const response = await openai.responses.create({
         input: [
-        //    { role : 'system', content: 'answer in 20 words'},
-        //    {role: 'developer', content: 'Give a basic example in js'},
            { role : 'user', content: prompt}
         ],
-        model
+        model,
+        // temperature: 2
+        // max_output_tokens: 16
+        store: true
     })
 
+    // console.log(response)
 
-    console.log(response.usage)
-
-    function calculateToken(){
-        const encoder = encoding_for_model(model);
-        const tokenData = encoder.encode(prompt);
-        console.log(tokenData);
-    }
-
-    calculateToken();
+    const oldResponse = await openai.responses.retrieve('resp_027e4e4147b44842006a22f33e50108193a1d630e72b37930b');
+    console.log(oldResponse)
 }
 
 main().catch(err => {
