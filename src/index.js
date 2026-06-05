95cd6508e114093b58cd34f95f4aaaa85487edc8
Haider Malik <haidermalik504@gmail.com>
2026-06-05 17:56:43 +0500

    understood how chat gpt claculate tokens and pricing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {openai} from './openai.js'
+import {encoding_for_model} from 'tiktoken';
 
 async function main(){
    
@@ -16,18 +17,33 @@ async function main(){
     // });
 
 
+    const prompt = `Many words map to one token, but some don't: indivisible.
+
+Unicode characters like emojis may be split into many tokens containing the underlying bytes: 🤚🏾
+
+Sequences of characters commonly found next to each other may be grouped together: 1234567890`;
+    const model = 'gpt-4o-mini'
+
 
     const response = await openai.responses.create({
         input: [
-           { role : 'system', content: 'answer in urdu language'},
-           {role: 'developer', content: 'Give a basic example in js'},
-           { role : 'user', content: 'what is coding'}
+        //    { role : 'system', content: 'answer in 20 words'},
+        //    {role: 'developer', content: 'Give a basic example in js'},
+           { role : 'user', content: prompt}
         ],
-        model: 'gpt-4o-mini'
+        model
     })
 
 
-    console.log(response.output_text)
+    console.log(response.usage)
+
+    function calculateToken(){
+        const encoder = encoding_for_model(model);
+        const tokenData = encoder.encode(prompt);
+        console.log(tokenData);
+    }
+
+    calculateToken();
 }
 
 main().catch(err => {
