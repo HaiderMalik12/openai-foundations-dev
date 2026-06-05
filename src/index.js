7581a18ba1f319c3b2b36c49bf8f4ee39c1e80d1
Haider Malik <haidermalik504@gmail.com>
2026-06-05 13:11:43 +0500

    instructions done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import {openai} from './openai.js'
 
 async function main(){
    
+    // const response = await openai.responses.create({
+    //     input: '10*10*23',
+    //     instructions: 'Give result in 1 word',
+    //     model: 'gpt-4o-mini',
+    // });
+
+
+    // const response = await openai.responses.create({
+    //     input: 'tell mw one best color',
+    //     instructions: 'Give result in 20 word',
+    //     model: 'gpt-4o-mini',
+    // });
+
+
+
     const response = await openai.responses.create({
-        input: 'apple color is',
+        input: [
+           { role : 'system', content: 'answer in urdu language'},
+           {role: 'developer', content: 'Give a basic example in js'},
+           { role : 'user', content: 'what is coding'}
+        ],
         model: 'gpt-4o-mini'
-    });
+    })
+
 
     console.log(response.output_text)
 }
