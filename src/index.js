70a9d5d09140d90097a6d6ec25d84bb209d3f779
Haider Malik <haidermalik504@gmail.com>
2026-06-06 20:56:51 +0500

    managed context

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import { openai } from "./openai.js";
 
+const context = [
+  { 
+    role : 'system',
+    content: 'Keep answer short and simple'
+  }
+]
+
 async function aiAnswer(question) {
+  
+  context.push({role: 'user', content: question});
+
   const response = await openai.responses.create({
     model: "gpt-4o-mini",
-    input: question,
+    input: context,
   });
 
+  context.push({role: 'assistant', content: response.output_text});
+  console.log(context);
   console.log(response.output_text);
 }
 
-process.stdout.write("Ask your question");
+process.stdout.write("Ask your question: ");
 
 process.stdin.on("data", (data) => {
   const question = data.toString().trim();
